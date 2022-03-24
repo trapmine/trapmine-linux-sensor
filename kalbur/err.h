1361e5c021ffac469a887713b3e893c9679b2e65
Usama Saqib <usama@trapmine.com>
2022-03-24 13:17:53 +0300

    CODE_SUCCESS value changed to 1

--- kalbur/err.h
+++ kalbur/err.h
@@ -16,7 +16,7 @@
 #endif
 
 #define CODE_FAILED -1
-#define CODE_SUCCESS 0
+#define CODE_SUCCESS 1
 #define CODE_RETRY -2
 
 #define ERR_NOT_SUCCESS(err) ((err == CODE_FAILED) || (err == CODE_RETRY))
