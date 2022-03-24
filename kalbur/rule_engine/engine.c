bba541c8a446ef0703d0050d570dc6fb1ee6bd9c
Usama Saqib <usama@trapmine.com>
2022-03-24 17:53:02 +0300

    driver code for engine

--- kalbur/rule_engine/engine.c
+++ kalbur/rule_engine/engine.c
@@ -1,16 +1,47 @@
+#include <engine.h>
+#include <populate.h>
 #include <context_manager.h>
-#include "engine.h"
+#include <stdbool.h>
+#include <save_ms.h>
+#include <err.h>
 
-void process_message(struct message_state *ms, safetable_t *table)
+static bool should_manage_ctx(struct message_state *ms)
 {
-	return;
-	//int err;
+	ASSERT(IS_MS_COMPLETE(ms) != 0,
+	       "should_manage_ctx: ms->progress != completed");
 
-	//if (should_manage_ctx(ms)) {
-	//	err = manage_process_context(table, ms);
-	//	transition_progress_state(ms);
-	//}
+	if (!(IS_MS_CTX_SAVED(ms) || IS_MS_IGNORE_CTX_SAVE(ms))) {
+		return true;
+	}
 
-	// TODO: save message into database
-	// TODO: transition message state
+	return false;
+}
+
+static bool should_save_db(struct message_state *ms)
+{
+	ASSERT(IS_MS_COMPLETE(ms) != 0,
+	       "should_manage_ctx: ms->progress != completed");
+	ASSERT(IS_MS_GC(ms) == 0, "should_save_db: ms->progress == gc");
+
+	if (!(IS_MS_DB_SAVED(ms))) {
+		return true;
+	}
+
+	return false;
+}
+
+void process_message(struct message_state *ms, sqlite3 *db, hashtable_t *ht,
+		     safetable_t *table)
+{
+	int err;
+
+	if (should_manage_ctx(ms)) {
+		err = manage_process_context(table, ms);
+		transition_ms_progress(ms, MS_CTX_SAVED, err);
+	}
+
+	if (should_save_db(ms)) {
+		err = save_msg(db, ht, ms);
+		transition_ms_progress(ms, MS_DB_SAVED, err);
+	}
 }
--- kalbur/rule_engine/engine.h
+++ kalbur/rule_engine/engine.h
@@ -1,8 +1,11 @@
 #ifndef ENGINE_H
-#define ENGINE
+#define ENGINE_H
 #include <safe_hash.h>
 #include <message.h>
+#include <hash.h>
+#include <sqlite3.h>
 
-void process_message(struct message_state *ms, safetable_t *table);
+void process_message(struct message_state *ms, sqlite3 *db, hashtable_t *ht,
+		     safetable_t *table);
 
 #endif // ENGINE_H
