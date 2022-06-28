d93121c31d4853721bfd6d4b6a1f2b68105c27f3
Muhammad Saboor <muhammad.saboor@trapmine.com>
2022-06-28 16:02:30 +0300

    Move notifier out of db directory

--- kalbur/rule_engine/database/notifier.h
+++
@@ -1,9 +0,0 @@
-#ifndef NOTIFIER_H
-#define NOTIFIER_H
-
-#include <hash.h>
-#include <sqlite3.h>
-
-int init_notifier(sqlite3 *db, hashtable_t *ht);
-
-#endif // NOTIFIER_H
--- kalbur/rule_engine/notifier.h
+++ kalbur/rule_engine/notifier.h
@@ -10,6 +10,24 @@
 #include "err.h"
 #include "message.h"
 
+struct alert_data {
+	uint64_t alert_type;
+};
+
+struct hashlookup_data {
+	uint64_t file_id;
+};
+
+struct action_struct {
+	uint64_t event_id;
+	uint64_t action_type;
+	union action_data {
+		struct alert_data alert;
+		struct hashlookup_data hashlookup;
+	} data;
+};
+
+int generate_alert(uint64_t event_id, uint64_t alert_type);
 int process_tags(struct message_state *ms);
 
 #endif // NOTIFIER_H
